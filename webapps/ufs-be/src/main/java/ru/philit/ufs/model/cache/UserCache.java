622106b9978e364306dcc7679f00fbc4e435c4ea
Ilev Andrei <ilev.andrei@yandex.ru>
2022-02-22 21:52:51 +0300

    Initial commit

---
+++ webapps/ufs-be/src/main/java/ru/philit/ufs/model/cache/UserCache.java
@@ -0,0 +1,22 @@
+package ru.philit.ufs.model.cache;
+
+import ru.philit.ufs.model.entity.user.ClientInfo;
+import ru.philit.ufs.model.entity.user.Operator;
+import ru.philit.ufs.model.entity.user.User;
+
+/**
+ * Интерфейс доступа к кешу данных для пользователей.
+ */
+public interface UserCache {
+
+  User getUser(String sessionId);
+
+  Operator getOperator(String userLogin, ClientInfo clientInfo);
+
+  Operator getOperatorById(String id, ClientInfo clientInfo);
+
+  void addUser(String sessionId, User user);
+
+  boolean removeUser(String sessionId);
+
+}
