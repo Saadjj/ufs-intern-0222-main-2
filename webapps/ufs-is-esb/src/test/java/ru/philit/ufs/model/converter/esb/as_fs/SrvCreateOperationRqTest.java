aa9411015587f8fd59d46f2407b7d7075e13ec31
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2022-03-14 04:53:19 +0200

    SrvCreateOperationRqTest updated

--- webapps/ufs-is-esb/src/test/java/ru/philit/ufs/model/converter/esb/as_fs/SrvCreateOperationRqTest.java
+++ webapps/ufs-is-esb/src/test/java/ru/philit/ufs/model/converter/esb/as_fs/SrvCreateOperationRqTest.java
@@ -4,39 +4,22 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.philit.ufs.model.entity.common.OperationTypeCode;
+import ru.philit.ufs.model.entity.esb.as_fs.OperTypeLabel;
 import ru.philit.ufs.model.entity.esb.as_fs.SrvCreateOperationRq;
 import ru.philit.ufs.model.entity.oper.Operation;
 
-import static ru.philit.ufs.model.entity.esb.as_fs.OperTypeLabel.TO_CARD_DEPOSIT;
-import static ru.philit.ufs.model.entity.esb.as_fs.OperTypeLabel.FROM_CARD_WITHDRAW;
+public class SrvCreateOperationRqTest extends AsFsAdapterTest {
 
-public class SrvCreateOperationRqTest extends AsFsAdapterTest{
+    private static final String WORK_PLACE_UID = "125";
+    private static final String OPERATOR_ID = "129";
 
-
-
-
-    private static final String workPlace_UID="125";
-    private static final String OPERATOR_ID="129";
-
-    private SrvCreateOperationRq request;
-    private Operation operation=new Operation();
-
-
+    private final Operation operation = new Operation();
 
     @Before
     public void setUp() {
-        request = new SrvCreateOperationRq();
-        request.setHeaderInfo(headerInfo());
-        request.setSrvCreateOperationRqMessage(new SrvCreateOperationRq.SrvCreateOperationRqMessage());
-        request.getSrvCreateOperationRqMessage().setOperatorId(OPERATOR_ID);
-        request.getSrvCreateOperationRqMessage().setWorkPlaceUId(workPlace_UID);
-        request.getSrvCreateOperationRqMessage().setOperationType(TO_CARD_DEPOSIT);
-                request.getSrvCreateOperationRqMessage().setOperationType(ConvectorsAsFs.operTypeLabel(OperationTypeCode.FROM_CARD_WITHDRAW));
         operation.setOperatorId(OPERATOR_ID);
-        operation.setWorkPlaceUId(workPlace_UID);
+        operation.setWorkPlaceUId(WORK_PLACE_UID);
         operation.setTypeCode(OperationTypeCode.FROM_CARD_WITHDRAW);
-
-
     }
 
     @Test
@@ -44,9 +27,9 @@ public class SrvCreateOperationRqTest extends AsFsAdapterTest{
         SrvCreateOperationRq request = SrvCreateOperationRqAdapter.requestCreateOperation(operation);
         assertHeaderInfo(request.getHeaderInfo());
         Assert.assertNotNull(request.getSrvCreateOperationRqMessage());
-        Assert.assertEquals(request.getSrvCreateOperationRqMessage().getOperatorId(),OPERATOR_ID);
-        Assert.assertEquals(request.getSrvCreateOperationRqMessage().getWorkPlaceUId(),workPlace_UID);
-        Assert.assertEquals(request.getSrvCreateOperationRqMessage().getOperationType(),FROM_CARD_WITHDRAW);
-
+        Assert.assertEquals(request.getSrvCreateOperationRqMessage().getOperatorId(), OPERATOR_ID);
+        Assert.assertEquals(request.getSrvCreateOperationRqMessage().getWorkPlaceUId(), WORK_PLACE_UID);
+        Assert.assertEquals(
+            request.getSrvCreateOperationRqMessage().getOperationType(), OperTypeLabel.FROM_CARD_WITHDRAW);
     }
 }
